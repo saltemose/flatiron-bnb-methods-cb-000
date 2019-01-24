a9ec43482671bebd5339793132580fd12fae9c79
Sean Altemose <sean@seanaltemose.com>
2019-01-24 19:44:05 +0000

    Done.

---
+++ app/models/concerns/city_and_neighborhood_helpers.rb
@@ -0,0 +1,18 @@
+module CityAndNeighborhoodHelpers
+
+  module InstanceMethods
+    def city_openings(start_date, end_date)
+    end
+  end
+
+  module ClassMethods
+    def highest_ratio_res_to_listings
+    end
+
+    def most_res
+    end
+  end
+
+
+
+end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,37 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, :checkout, presence: true
+  validate :self_reservation
+  validate :available
+  validate :checkout_after_checkin
+  validate :checkin_not_checkout_dates
+
+  def self_reservation
+    if listing.host == guest
+      errors.add(:guest_id, "Cannot book your own listing!")
+    end
+  end
+
+  def available
+    if (!checkin.nil? && !checkout.nil?)
+      self.listing.reservations.each do |r|
+        if (checkin <= r.checkout) && (checkout >= r.checkin)
+          errors.add(:checkin, "listing is not available")
+        end
+      end
+    end
+  end
+
+  def checkout_after_checkin
+    if checkout && checkin && checkin > checkout
+      errors.add(:guest_id, "Your checkout needs to be after your checkin.")
+    end
+  end
+
+  def checkin_not_checkout_dates
+    if checkin == checkout
+      errors.add(:guest_id, "Your checkin and checkout dates cannot be the same.")
+    end
+  end
 end
