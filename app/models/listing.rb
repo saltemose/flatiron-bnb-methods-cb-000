387c3f6a566c8e67db468ae7fe19cf3f5b656595
Sean Altemose <sean@seanaltemose.com>
2019-01-25 00:16:30 +0000

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,7 +4,15 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
+  has_many :ratings, :through => :reviews
 
-  validates :address, :listing_type, :title, :description, :price, :neighborhood_id, presence: true 
+  validates :address, :listing_type, :title, :description, :price, :neighborhood_id, presence: true
 
+  def average_review_rating
+    sum = 0
+    self.ratings.each do |rating|
+      sum = rating + sum
+    end
+    sum/self.ratings.count
+  end
 end
